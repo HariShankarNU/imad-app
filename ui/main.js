d92160f9c8ee7289ca5704355fc885d611196598
HariShankarNU <30701773+HariShankarNU@users.noreply.github.com>
2017-08-09 23:48:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element=document.getElementById('main');
 element.innerHTML="new";
+
+var image=document.getElementById('ima');
+
+image.onClick=function(){
+    image.style.marginleft="100px";
+};
